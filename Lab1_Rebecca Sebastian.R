acaaeeac5987f54318228453b0a3350f9030dbbc
Rebecca Sebastian <rsebastian@umass.edu>
2020-09-03 00:05:51 +0000

    commit

---
+++ Lab1_Rebecca Sebastian.R
@@ -0,0 +1,30 @@
+## Title:Lab 1
+## Author: Rebecca Sebastian
+
+### Exercise 1
+
+x<- 3
+y<- 15
+x + y 
+x - y
+x * y
+y / x
+
+### Exercise 2
+
+x<- 2
+y<- 3
+x^5 ; y^4
+z = x > y
+z
+
+### Exercise 3
+
+x<- c(22,62,148,43,129)
+sum (x)
+x / sum (x)
+
+### Exercise 4
+
+nucleotides<- c("A","T","C","G")
+sort (nucleotides)
